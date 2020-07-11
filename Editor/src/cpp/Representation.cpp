4cd86f62774363ea8bc5bad2d59ff19c4a20828b
Alex Frederiksen <ajfred154@gmail.com>
2020-07-11 15:13:51 -0400

    Add Representation.cpp for happy compiling

---
+++ Editor/src/cpp/Representation.cpp
@@ -0,0 +1,14 @@
+#include "Representation.hpp"
+
+Representation::Representation() {}
+
+//function types probably need to change
+
+void Representation::add_circle(circle_t) {}
+
+void Representation::cvx_hull() {}
+
+void Representation::compute_implications() {}
+
+void Representation::compute_cvx_geo() {}
+
